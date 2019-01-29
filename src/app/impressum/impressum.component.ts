886a5c0f274092bb95d2294cf97595b6bdb982f4
Jens Winter <jens.winter@gmail.com>
2019-01-29 21:59:18 +0100

    Hilfe-Seite

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
-  selector: 'app-impressum',
-  templateUrl: './impressum.component.html',
-  styleUrls: ['./impressum.component.css']
+    selector: 'app-impressum',
+    templateUrl: './impressum.component.html',
+    styleUrls: ['./impressum.component.css']
 })
-export class ImpressumComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ImpressumComponent {
 }
