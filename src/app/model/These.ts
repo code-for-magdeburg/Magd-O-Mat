e63f9f4e4ae03c3038d4cf04da62845f252de3d4
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-01-21 10:11:37 +0100

    Hintergrundinformationen können angezeigt werden

--- src/app/model/These.ts
+++ src/app/model/These.ts
@@ -3,6 +3,13 @@ export type TheseWertung = 'ja' | 'neutral' | 'nein' | 'ohne';
 export type These = {
   kategorie: string;
   text: string;
+  infos: Info[];
+};
+
+export type Info = {
+  titel: string;
+  quelle: string;
+  link: string;
 };
 
 export type TheseEingabe = {
